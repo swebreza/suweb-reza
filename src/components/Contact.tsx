7bf29f488534632b62f09b30a47fe2ffa5602c51
MD Suweb Reza <45898572+swebreza@users.noreply.github.com>
2024-09-11 14:16:06 +0530

    project & contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,17 +1,99 @@
-export default function Contact() {
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  // CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+// import { Input } from '@/components/ui/input'
+// import { Label } from '@/components/ui/label'
+// import { Textarea } from '@/components/ui/textarea'
+import { MapPin, Phone, Mail, Github, Linkedin, Twitter } from 'lucide-react'
+
+export default function ContactPage() {
   return (
-    <section id='contact' className='p-10 bg-gray-950 text-center'>
-      <h2 className='text-4xl font-bold mb-6'>Contact Me</h2>
-      <p>
-        You can reach me at{' '}
-        <a
-          href='mailto:shivam@example.com'
-          className='text-blue-500 hover:underline'
-        >
-          swebreza@gmail.com
-        </a>
-        .
-      </p>
-    </section>
+    <div className='container mx-auto px-4 py-8'>
+      <h1 className='text-4xl font-bold text-center mb-8'>Get in Touch</h1>
+      <div className='space-y-8'>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
+          <Card>
+            <CardHeader>
+              <CardTitle className=' text-white'>Contact Information</CardTitle>
+              <CardDescription className=' text-white'>
+                Feel free to reach out through any of these channels.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className='space-y-4'>
+              <div className='flex items-center space-x-4'>
+                <MapPin className='text-muted-foreground ' color='white' />
+                <span>15th cross, Lakkasandra, Bangalore - 560030</span>
+              </div>
+              <div className='flex items-center space-x-4'>
+                <Phone className='text-muted-foreground' />
+                <span>+91 9538774092</span>
+              </div>
+              <div className='flex items-center space-x-4'>
+                <Mail className='text-muted-foreground' />
+                <span>swebreza@gmail.com</span>
+              </div>
+            </CardContent>
+          </Card>
+
+          <Card>
+            <CardHeader>
+              <CardTitle className=' text-white'>Connect with Me</CardTitle>
+              <CardDescription className=' text-white'>
+                Follow me on social media for updates.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className='flex justify-center space-x-4'>
+              <a href='https://github.com/swebreza' target='_blank'>
+                <Button variant='outline' size='icon'>
+                  <Github className='h-4 w-4' />
+                </Button>
+              </a>
+              <a href='https://www.linkedin.com/in/suwebreza/' target='_blank'>
+                <Button variant='outline' size='icon'>
+                  <Linkedin className='h-4 w-4' />
+                </Button>
+              </a>
+              <a
+                href='https://x.com/SwebReza'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <Button variant='outline' size='icon'>
+                  <Twitter className='h-4 w-4' />
+                </Button>
+              </a>
+            </CardContent>
+          </Card>
+        </div>
+        <Card className='h-fit'>
+          <CardHeader>
+            <CardTitle className=' text-white'>Location</CardTitle>
+            <CardDescription className=' text-white'>
+              Find me on the map
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className=' bg-muted flex items-center justify-center'>
+              {/* <MapPin className='text-muted-foreground' /> */}
+              <iframe
+                src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d972.1121043563569!2d77.59851466962257!3d12.94313300624192!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae15b7b364e129%3A0x45add615f7cd8e71!2sFamily%20Supermarket!5e0!3m2!1sen!2sin!4v1726043035875!5m2!1sen!2sin'
+                width='900'
+                height='300'
+                style={{ border: 0 }}
+                allowFullScreen={true}
+                loading='lazy'
+                referrerPolicy='no-referrer-when-downgrade'
+              ></iframe>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
   )
 }
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,18 +1,83 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import Image, { StaticImageData } from 'next/image'
+import Nikkari from '@/assets/nikkari.png'
+import Portfolio from '@/assets/portfolio.png'
+import TravelApp from '@/assets/travel.png'
+
+// Define the structure of a project
+interface Project {
+  id: number
+  title: string
+  description: string
+  imageUrl: StaticImageData
+  link?: string
+}
+
+// Sample project data
+const projects: Project[] = [
+  {
+    id: 1,
+    title: 'Nikkari',
+    description:
+      'Nikkari developed brush cutters to reduce mowing work under the hot sun, and mono-rack to ease heavy items carrying in steep field.',
+    imageUrl: Nikkari,
+    link: 'https://nikkari.vercel.app/',
+  },
+  {
+    id: 2,
+    title: 'Portfolio',
+    description: 'Personal portfolio website',
+    imageUrl: Portfolio,
+    link: 'https://main--cheery-scone-e484f6.netlify.app/',
+  },
+  {
+    id: 3,
+    title: 'Travel App',
+    description: 'Travel app to find the best places to visit',
+    imageUrl: TravelApp,
+    link: 'https://distracted-euler-c04064.netlify.app/',
+  },
+  // {
+  //   id: 4,
+  //   title: 'Project Delta',
+  //   description: 'E-commerce platform for small businesses',
+  //   imageUrl: '/placeholder.svg?height=200&width=300',
+  //   link: 'https://www.google.com',
+  // },
+]
+
 export default function Projects() {
   return (
-    <section id='projects' className='p-10 bg-gray-900'>
-      <h2 className='text-4xl font-bold text-center mb-8'>Projects</h2>
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
-        {/* Repeat the project card as needed */}
-        <div className='p-6 border rounded-md shadow-md'>
-          <h3 className='text-2xl font-bold mb-4'>Project 1</h3>
-          <p className='mb-4'>
-            Description of Project 1 with its details and technologies used.
-          </p>
-          <a href='#' className='text-blue-500 hover:underline'>
-            View Project
-          </a>
-        </div>
+    <section id='projects' className='p-10 rounded-3xl bg-black'>
+      <h2 className='text-4xl font-bold text-white'>Project</h2>
+      <span className='px-2 mb-6 bg-slate-800 text-gray-800 rounded-full'>
+        -----------
+      </span>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 overflow-hidden'>
+        {projects.map((project) => (
+          <Card key={project.id} className='overflow-hidden'>
+            <a href={project?.link}>
+              <Image
+                src={project.imageUrl}
+                alt={project.title}
+                width={300}
+                height={200}
+                className='w-full h-48 object-cover'
+              />
+              <CardHeader>
+                <CardTitle className='text-white'>{project.title}</CardTitle>
+                <CardDescription className='text-gray-400'>
+                  {project.description}
+                </CardDescription>
+              </CardHeader>
+            </a>
+          </Card>
+        ))}
       </div>
     </section>
   )
